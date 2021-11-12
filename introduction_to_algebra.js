67c8837acee88419d0fcb8e68cb25ec772d7dc4b
Niño James A. Tan <njtan142@gmail.com>
2021-11-12 20:51:04 +0800

    Evaluate algebraic expressions

--- introduction_to_algebra.js
+++ introduction_to_algebra.js
@@ -15,7 +15,6 @@ function algebra_simplify() {
         previous = equation[i]
 
     }
-    console.log(correct)
     equation = correct
     let simplified = math.rationalize(math.simplify(equation))
     let result = simplified.toString()
@@ -33,7 +32,36 @@ function algebra_simplify() {
 function algebra_evaluate() {
     var parent = document.getElementById("algebra_evaluate")
     let equation = parent.children[1].value
+    let previous = ""
+    let correct = ""
+    for (i = 0; i < equation.length; i++) {
+        //check if the previous and current character are letters
+
+        if (previous.match(/[a-z]/i) && equation[i].match(/[a-z]/i)) {
+            //check if the two letters are the same
+            correct += "*" + equation[i]
+        }else{
+            correct += equation[i]
+        }
+        previous = equation[i]
+
+    }
+    equation = correct
+    let simplified = math.rationalize(math.simplify(equation))
     let values = parent.children[2].value
+    // remove spaces of value
+    while (values.includes(" ")) {
+        values = values.replace(" ", "")
+    }
+    // convert values into an objectlet values_object = {}
+    let values_object = {}
+    let values_array = values.split(",")
+    for (i = 0; i < values_array.length; i++) {
+        values_object[values_array[i].split("=")[0]] = parseFloat(values_array[i].split("=")[1])
+    }
+    console.log(simplified.toString(), values_object)
 
-
+    // evaluate the equation
+    let result = math.evaluate(simplified.toString(), values_object)
+    parent.children[3].innerHTML = result.toString()
 }
