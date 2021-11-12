c543b3bfa84105692915885874235f979274a77b
Niño James A. Tan <njtan142@gmail.com>
2021-11-12 15:14:07 +0800

    algebraic expression correction on variables

--- introduction_to_algebra.js
+++ introduction_to_algebra.js
@@ -1,15 +1,39 @@
-function algebra_simplify(){
+function algebra_simplify() {
     var parent = document.getElementById("algebra_simplify")
     let equation = parent.children[1].value
-    console.log(equation)
-    let simplified = math.simplify(math.rationalize(equation))
+    let previous = ""
+    let correct = ""
+    for (i = 0; i < equation.length; i++) {
+        //check if the previous and current character are letters
+
+        if (previous.match(/[a-z]/i) && equation[i].match(/[a-z]/i)) {
+            //check if the two letters are the same
+            correct += "*" + equation[i]
+        }else{
+            correct += equation[i]
+        }
+        previous = equation[i]
+
+    }
+    console.log(correct)
+    equation = correct
+    let simplified = math.rationalize(math.simplify(equation))
     let result = simplified.toString()
 
-    while(result.includes(" * ")){
+    while (result.includes(" * ")) {
         //replace all instances of * 
         result = result.replace(" * ", "")
     }
 
 
     parent.children[2].innerHTML = result
+}
+
+
+function algebra_evaluate() {
+    var parent = document.getElementById("algebra_evaluate")
+    let equation = parent.children[1].value
+    let values = parent.children[2].value
+
+
 }
